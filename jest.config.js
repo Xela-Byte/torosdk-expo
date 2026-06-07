e01e49d5734d23bd8e2edb0434da4a416bb458c3
Torosdk Dev <dev@torosdk.dev>
2026-06-07 17:47:18 +0100

    chore: scaffold project with package.json, tsconfig, jest config

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  moduleNameMapper: {
+    '^torosdk-expo$': '<rootDir>/src/react/index.ts',
+    '^torosdk-expo/core$': '<rootDir>/src/core/index.ts',
+    '^torosdk-expo/cli$': '<rootDir>/src/cli/init.ts',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+        esModuleInterop: true,
+        strict: true,
+        module: 'commonjs',
+        target: 'ES2020',
+        moduleResolution: 'node',
+      }
+    }],
+  },
+};
